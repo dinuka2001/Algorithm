4989f9a5a2b00fbc71fe6fbcb95b41f8205c49c9
dinuka2001 <dinukasahan2001@gmail.com>
2024-09-11 12:07:46 +0530

    Refactor code to initialize matrix with -1 and add comments for better understanding

--- Longest_commen_subsequence.c
+++ Longest_commen_subsequence.c
@@ -10,7 +10,7 @@ int max(int a, int b)
 int LCS(char str1[], char str2[], int m, int n)
 {
 
-    //create mattrix
+    //create mattrix and initialize with -1
     int mattrix[m + 1][n + 1];
     for (int i=0; i<=m; i++){
         for (int j=0; j<=n; j++){
@@ -22,14 +22,17 @@ int LCS(char str1[], char str2[], int m, int n)
     {
         return 0;
     }
-    if (mattrix[m][n] != -1)
+    //if value is already calculated then return it
+    if (mattrix[m][n] != -1)//is [m][n] have some value?
     {
         return mattrix[m][n];
     }
+    //if last character of the substring is same?  then add 1 and call LCS for m-1 and n-1
     if (str1[m] == str2[n])
     {
         return mattrix[m][n] = 1 + LCS(str1, str2, m - 1, n - 1);
     }
+    //if last character of the substring is not same? then get max of LCS for m-1 and & LCS for m and n-1
     else
     {
         return mattrix[m][n] = max(LCS(str1, str2, m - 1, n), LCS(str1, str2, m, n - 1));
@@ -38,15 +41,24 @@ int LCS(char str1[], char str2[], int m, int n)
 
 int main()
 {
-
+    //declare two strings with default values
     char str1[] = "AGGTAB";
     char str2[] = "GXTXAYB";
 
+    //take input from user
+    printf("Enter the first string: ");
+    scanf("%s", str1);
+    printf("Enter the second string: ");
+    scanf("%s", str2);
+
+    //get length of the strings
     int m = strlen(str1);
     int n = strlen(str2);
 
+    //call LCS function
     int lcsLength = LCS(str1, str2, m, n);
 
+    //print the length of the longest common subsequence
     printf("Length of Longest Common Subsequence: %d\n", lcsLength);
 
 
